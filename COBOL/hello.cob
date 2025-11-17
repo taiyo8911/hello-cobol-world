6b7539193ced4e37afd5dd7612529366d80688f6
SolSE <68884687+taiyo8911@users.noreply.github.com>
2025-11-17 20:00:53 +0900

    Add files via upload

---
+++ COBOL/hello.cob
@@ -0,0 +1,8 @@
+       IDENTIFICATION  DIVISION.
+       PROGRAM-ID.     HELLO_WORLD.
+       DATA            DIVISION.
+       WORKING-STORAGE SECTION.
+       01  NAME-VAR    PIC X(10) VALUE "taiyo".
+       PROCEDURE       DIVISION.
+          DISPLAY  "HELLO " NAME-VAR
+          STOP RUN.
